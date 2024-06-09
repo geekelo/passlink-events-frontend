5a03b1547a5b0617e0decf5f6fe8f9ed90e629ef
geekelo <efueliht12@gmail.com>
2024-06-09 18:41:52 +0100

    Add structure for event card

---
+++ src/components/events/eventCard.js
@@ -0,0 +1,20 @@
+function EventCard() {
+  return (
+    <div>
+      <div>
+        <p>TITLE</p>
+        <p>DESCRIPTION</p>
+        <p>Organizers</p>
+      </div>
+      <div>
+        <p>LOCATION</p>
+        <p>DATE</p>
+        <p>TIME</p>
+        <p>View</p>
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
+// Path: src/components/events/eventCards.js
--- src/components/events/eventsPage.js
+++ src/components/events/eventsPage.js
@@ -1,10 +1,14 @@
 import FilterSection from '../nav/filterSection';
+import EventCard from './eventCard';
 
 function EventsPage() {
   return (
     <div className="eventsPage">
       <div className="filterSection">
         <FilterSection />
+      </div>
+      <div>
+        <EventCard />
       </div>
     </div>
   );
