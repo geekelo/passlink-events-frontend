0ba03f8082a6d6a59eb60634469ca33a4976dc4c
geekelo <efueliht12@gmail.com>
2024-06-11 20:00:01 +0100

    Add event data

--- src/components/events/eventsPage.js
+++ src/components/events/eventsPage.js
@@ -1,12 +1,19 @@
 import FilterSection from '../nav/filterSection';
 import EventCard from './eventCard';
 import '../../stylesheets/desktop/eventPage.css';
+import events from '../../data/events.json';
 
 function EventsPage() {
+  const eventData = events.data;
   return (
     <div className="eventsPage">
       <div className="filter-section">
-        <FilterSection />
+        {
+          eventData.map((event) => (<FilterSection
+            key={event.id}
+            event={event}
+          />))
+        }
       </div>
       <div>
         <EventCard />
